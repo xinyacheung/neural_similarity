8c384a5fe3dcbebcaa0484e9d8efb6f7f68641f3
xinyacheung <xinyazhang08@gmail.com>
2023-09-05 20:52:31 +0100

    main

--- NNmodel/MyDataset.py
+++ NNmodel/MyDataset.py
@@ -2,28 +2,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.utils.data import Dataset
-
 import torchvision.transforms.functional as TF
-
 from PIL import Image
-
-class CNNDataset(Dataset):
-
-  def __init__(self, path:list, classes:list, transform=None):
-
-    self.images = path
-    self.label = classes
-
-  def __getitem__(self, index):
-
-    img = TF.to_tensor(Image.open(self.images[index]).convert('RGB'))
-    train_X = img
-    train_Y = self.label[index]
-
-    return train_X, train_Y
-
-  def __len__(self):
-    return len(self.images)
 
 class LSTMDataset(Dataset):
 
